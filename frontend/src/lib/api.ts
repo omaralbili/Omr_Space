07e3979ede85aed0df4358179c638d6e6dd58f03
omaralbili <omar.elbialy2143@gmail.com>
2026-09-18 20:24:39 +0300

    Change API base URL to production endpoint

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "/api" });
+export const api = axios.create({ baseURL:"https:omr-space.vercel.app" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
